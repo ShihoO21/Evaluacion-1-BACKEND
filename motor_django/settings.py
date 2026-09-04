b5b1f84ae43c6fab6aab36be54e4588af6d6305a
ShihoO21 <sebastianparra2509@ejemplo.com>
2026-09-04 17:12:35 -0400

    listo para entrega

--- motor_django/settings.py
+++ motor_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8nlc8q0v7#5s9%e8$q49yupe2im^bwo$mo+ucf4#)m1aw1wyn8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- transporte/views.py
+++ transporte/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def bienvenida(request):
-    return render(request, 'bienvenida.html')# Create your views here.
+    return render(request, 'bienvenida.html')
